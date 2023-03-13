4246f81bde764dd07ba9e338d3e13e792cdaccf5
paxmiler <paxmiler@gmail.com>
2023-03-14 00:31:54 +0900

    Initial Commit

---
+++ AutoScrolling/AutoScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoScrollingApp.swift
+//  AutoScrolling
+//
+//  Created by Martin.Q on 2023/03/14.
+//
+
+import SwiftUI
+
+@main
+struct AutoScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
