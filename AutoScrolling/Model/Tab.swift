87d1ce778f1a7f4792bd8b906812b71a8d5e58db
paxmiler <paxmiler@gmail.com>
2023-03-14 01:19:16 +0900

    !! Project Generate
    + add Assets
    + Project setting

---
+++ AutoScrolling/Model/Tab.swift
@@ -0,0 +1,19 @@
+//
+//  Tab.swift
+//  AutoScrolling
+//
+//  Created by Martin.Q on 2023/03/14.
+//
+
+import SwiftUI
+
+/// Enum Tab Cases
+/// - rawValue: Asset Image Name
+
+enum Tab: String {
+	case dance = "Dance"
+	case fruite = "Fruite"
+	case mirror = "Mirror"
+	case night = "Night"
+	case road = "Road"
+}
---
+++ AutoScrolling/View/Home.swift
@@ -0,0 +1,25 @@
+//
+//  Home.swift
+//  AutoScrolling
+//
+//  Created by Martin.Q on 2023/03/14.
+//
+
+import SwiftUI
+
+struct Home: View {
+	/// View Properties
+	@State private var activeTab: Tab = .dance
+    var body: some View {
+		VStack {
+			
+		}
+		.padding()
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+		ContentView()
+    }
+}
